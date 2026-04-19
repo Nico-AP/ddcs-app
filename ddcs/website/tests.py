8e44eb81c762521dd74a693b65e7ec4428cdbbd7
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2026-04-19 21:05:57 +0200

    chore: extended basic tests to check availability of all implemented webpages.

--- ddcs/website/tests.py
+++ ddcs/website/tests.py
@@ -11,3 +11,21 @@ class WebsiteTests(TestCase):
         url = reverse("website:landing_page")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+    def test_impressum_page(self):
+        """Test that the imprint page returns a 200 status code."""
+        url = reverse("website:impressum")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_dps_page(self):
+        """Test that the data protection statement page returns a 200 status code."""
+        url = reverse("website:dps")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_dfdw_2025_page(self):
+        """Test that the DFDW 2025 page returns a 200 status code."""
+        url = reverse("website:dfdw_2025")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
