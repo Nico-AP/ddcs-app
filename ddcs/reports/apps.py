9459ab84d35d79d377c537761eb51ebb9e54315a
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2026-04-22 17:59:01 +0200

    chore: added boilerplate for reports app.

---
+++ ddcs/reports/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class DDCSReportsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ddcs.reports"
+    verbose_name = "DDCS Reports"
+    label = "ddcs_reports"
