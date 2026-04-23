b556ab71594e2449ef3509e2eab0a84d86c2e895
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2026-04-23 20:32:23 +0200

    feat: added two factor authentication for admin accounts.

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from two_factor.urls import urlpatterns as tf_urls
 
 urlpatterns = [
     path(f"{settings.ADMIN_URL}", admin.site.urls),
     path("", include("ddcs.website.urls", namespace="website")),
     path("", include("ddcs.datadonation.urls", namespace="datadonation")),
+    # 2FA
+    path("", include(tf_urls)),
     # DDM
     path("ddm/", include("ddm.core.urls")),
     path("ckeditor5/", include("django_ckeditor_5.urls")),
